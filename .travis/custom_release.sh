bf30524c34d1b2fd07e30c533c97430fd4857516
Ryan Long <rlong@redhat.com>
2020-03-06 10:40:34 -0500

    fix custom release

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -2,31 +2,28 @@
 set -e
 set -x
 
-if [ "${TRAVIS_BRANCH}" = "master" ]; then
+if [ "${TRAVIS_BRANCH}" = "master" ]
+then
     for env in ci qa
     do
-        echo
-        echo
         echo "PUSHING ${env}-beta"
-        rm -rf ./build/.git
+        rm -rf ./dist/.git
         .travis/release.sh "${env}-beta"
     done
 fi
 
-if [ "${TRAVIS_BRANCH}" = "master-stable" ]; then
+
+if [ "${TRAVIS_BRANCH}" = "master-stable" ]
+then
     for env in ci qa
     do
-        echo
-        echo
-        echo "PUSHING ${env}-beta"
-        rm -rf ./build/.git
+        echo "PUSHING ${env}-stable"
+        rm -rf ./dist/.git
         .travis/release.sh "${env}-stable"
     done
 fi
 
 if [[ "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" ]]; then
-    echo
-    echo
     echo "PUSHING ${TRAVIS_BRANCH}"
     rm -rf ./build/.git
     .travis/release.sh "${TRAVIS_BRANCH}"

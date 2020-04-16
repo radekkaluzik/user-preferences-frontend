65c20b4be8dc15ad74bda79bc413308b641509cb
Karel Hala <khala@redhat.com>
2020-04-16 10:37:24 +0200

    Redirect to root if user is not defined (#23)
    * Redirect to root if user is not defined
    
    * Use history replace

--- src/App.js
+++ src/App.js
@@ -1,44 +1,35 @@
-import PropTypes from 'prop-types';
-import React, { Component, Fragment } from 'react';
-import { withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React, { Fragment, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import { Routes } from './Routes';
 import { register } from './store';
 import { notifications } from '@redhat-cloud-services/frontend-components-notifications';
 import { NotificationsPortal } from '@redhat-cloud-services/frontend-components-notifications/';
 import './App.scss';
 
-class App extends Component {
+const App = (props) => {
+    const history = useHistory();
+    useEffect(() => {
+        (async () => {
+            const user = await insights.chrome.auth.getUser();
+            if (!user) {
+                history.replace('./');
+            }
+        })();
 
-    componentDidMount () {
         register({ notifications });
         insights.chrome.init();
         insights.chrome.identifyApp('user-preferences');
 
-        this.appNav = insights.chrome.on('APP_NAVIGATION', event => this.props.history.push(`/${event.navId}`));
-    }
+        const appNav = insights.chrome.on('APP_NAVIGATION', event => history.push(`/${event.navId}`));
+        return () => appNav();
+    }, []);
 
-    componentWillUnmount () {
-        this.appNav();
-    }
-
-    render () {
-        return (
-            <Fragment>
-                <NotificationsPortal />
-                <Routes childProps={ this.props } />
-            </Fragment>
-        );
-    }
-}
-
-App.propTypes = {
-    history: PropTypes.object
+    return (
+        <Fragment>
+            <NotificationsPortal />
+            <Routes childProps={ props } />
+        </Fragment>
+    );
 };
 
-/**
- * withRouter: https://reacttraining.com/react-router/web/api/withRouter
- * connect: https://github.com/reactjs/react-redux/blob/master/docs/api.md
- *          https://reactjs.org/docs/higher-order-components.html
- */
-export default withRouter (connect()(App));
+export default App;

66475054ea804ab593f07a062e0ec6b71352ab53
Karel Hala <khala@redhat.com>
2021-03-31 14:55:35 +0200

    When nav change received select correct page (#64)

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { register } from './store';
 import { notificationsReducer } from '@redhat-cloud-services/frontend-components-notifications/redux';
 import NotificationsPortal from '@redhat-cloud-services/frontend-components-notifications/NotificationPortal';
 import './App.scss';
+import config from './config/config.json';
 
 const App = (props) => {
   const history = useHistory();
@@ -20,9 +21,14 @@ const App = (props) => {
     insights.chrome.init();
     insights.chrome.identifyApp('email');
 
-    const appNav = insights.chrome.on('APP_NAVIGATION', (event) =>
-      history.push(`/${event.navId}`)
-    );
+    const appNav = insights.chrome.on('APP_NAVIGATION', (event) => {
+      const subNavItems = Object.keys(config?.['notification-preference']);
+      history.push(
+        `${subNavItems.includes(event.navId) ? '/notifications' : ''}/${
+          event.navId
+        }`
+      );
+    });
     return () => appNav();
   }, []);
 

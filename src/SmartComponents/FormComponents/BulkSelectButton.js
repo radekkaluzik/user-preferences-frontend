3cdcbe26d5bc57dffb9e05721ccf3c3fa87335d0
Filip Hlavac <filip.hlavac01@gmail.com>
2023-08-22 16:04:44 +0200

    Clean the BulkSelectButton

--- src/SmartComponents/FormComponents/BulkSelectButton.js
+++ src/SmartComponents/FormComponents/BulkSelectButton.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import { Button } from '@patternfly/react-core';
 import PropTypes from 'prop-types';
 import useFormApi from '@data-driven-forms/react-form-renderer/use-form-api';
@@ -8,38 +8,9 @@ import './BulkSelectButton.scss';
 
 const BulkSelectButton = (props) => {
   const formOptions = useFormApi();
-  const dirtyFieldsRef = useRef(formOptions.getState().dirtyFields);
-  const {
-    group,
-    section,
-    input: { ...input },
-  } = useFieldApi({
+  const { input, group, section } = useFieldApi({
     ...props,
     type: 'button',
-  });
-  useEffect(() => {
-    if (
-      JSON.stringify(dirtyFieldsRef.current) !==
-      JSON.stringify(formOptions.getState().dirtyFields)
-    ) {
-      let notIsSubscribed;
-      formOptions.batch(() => {
-        formOptions.getRegisteredFields().forEach((field) => {
-          if (
-            !field.includes(BULK_SELECT_BUTTON) &&
-            ((field.includes(group) && field.includes(section)) ||
-              (field === 'is_subscribed' && // a temporary condition for RHEL Advisor email pref.
-                group === 'rhel' &&
-                section == 'advisor')) &&
-            formOptions.getFieldState(field).value == false
-          ) {
-            notIsSubscribed = true;
-          }
-        });
-      });
-      formOptions.change(input.name, notIsSubscribed ?? false);
-      dirtyFieldsRef.current = formOptions.getState().dirtyFields;
-    }
   });
 
   return (
@@ -47,7 +18,7 @@ const BulkSelectButton = (props) => {
       className="pref-c-bulk-select-button"
       variant="secondary"
       {...input}
-      id={`button-${input.name}`}
+      id={`bulk-select-${section}`}
       onClick={() => {
         formOptions.batch(() => {
           formOptions.getRegisteredFields().forEach((field) => {
@@ -62,10 +33,10 @@ const BulkSelectButton = (props) => {
             }
           });
         });
-        formOptions.change(input.name, !input.value);
+        input.onChange(!input.value);
       }}
     >
-      {input.value == true ? 'Select all' : 'Deselect All'}
+      {input.value ? 'Select' : 'Deselect'} all
     </Button>
   );
 };
@@ -73,11 +44,6 @@ const BulkSelectButton = (props) => {
 BulkSelectButton.propTypes = {
   group: PropTypes.string,
   section: PropTypes.string,
-  name: PropTypes.string,
-};
-
-BulkSelectButton.defaultProps = {
-  name: '',
 };
 
 export default BulkSelectButton;
--- src/SmartComponents/FormComponents/BulkSelectButton.test.js
+++ src/SmartComponents/FormComponents/BulkSelectButton.test.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import { Form, RendererContext } from '@data-driven-forms/react-form-renderer';
-import { render } from '@testing-library/react';
+import { fireEvent, getByRole, render } from '@testing-library/react';
 import BulkSelectButton from './BulkSelectButton';
 
-describe('Bulk Select Button tests', () => {
+const mockInputOnChange = jest.fn();
+
+jest.mock('@data-driven-forms/react-form-renderer/use-field-api', () =>
+  jest.fn((props) => ({
+    ...props,
+    input: {
+      onChange: mockInputOnChange,
+      value: true,
+    },
+  }))
+);
+
+describe('BulkSelectButton tests', () => {
+  const onChangeMock = jest.fn();
+
+  afterEach(() => {
+    mockInputOnChange.mockReset();
+    onChangeMock.mockReset();
+  });
+
   it('should render correctly', () => {
     const { container } = render(
       <Form onSubmit={() => undefined}>
@@ -13,21 +32,48 @@ describe('Bulk Select Button tests', () => {
               formOptions: {
                 internalRegisterField: jest.fn(),
                 internalUnRegisterField: jest.fn(),
-                renderForm: () => {},
                 getState: () => ({
                   dirtyFields: [],
                 }),
               },
-              validatorMapper: {
-                required: () => (value) => value ? undefined : 'required',
-              },
             }}
           >
-            <BulkSelectButton label="test label" clearedValue {...props} />
+            <BulkSelectButton label="My button" {...props} />
           </RendererContext.Provider>
         )}
       </Form>
     );
     expect(container).toMatchSnapshot();
+  });
+
+  it('should toggle correctly', () => {
+    const { container } = render(
+      <Form onSubmit={() => undefined}>
+        {(props) => (
+          <RendererContext.Provider
+            value={{
+              formOptions: {
+                internalRegisterField: jest.fn(),
+                internalUnRegisterField: jest.fn(),
+                batch: (callback) => callback(),
+                getRegisteredFields: () => ['rhel[advisor]', 'rhel[sources]'],
+                change: onChangeMock,
+              },
+            }}
+          >
+            <BulkSelectButton
+              label="My button"
+              group="rhel"
+              section="sources"
+              {...props}
+            />
+          </RendererContext.Provider>
+        )}
+      </Form>
+    );
+
+    fireEvent.click(getByRole(container, 'button'));
+    expect(onChangeMock).toHaveBeenCalledWith('rhel[sources]', true);
+    expect(mockInputOnChange).toHaveBeenCalledWith(false);
   });
 });

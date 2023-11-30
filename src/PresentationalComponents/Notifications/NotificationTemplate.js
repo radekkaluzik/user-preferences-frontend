59063c295df82578a3bae6c7d4425ddc4e24ba3c
Filip Hlavac <filip.hlavac01@gmail.com>
2023-11-30 14:42:48 +0100

    Warn user on unsaved changes

--- src/PresentationalComponents/Notifications/NotificationTemplate.js
+++ src/PresentationalComponents/Notifications/NotificationTemplate.js
@@ -1,17 +1,76 @@
-import React from 'react';
-import { useFormApi } from '@data-driven-forms/react-form-renderer';
+import React, { useCallback, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { useFormApi } from '@data-driven-forms/react-form-renderer';
+import WarningModal from '@patternfly/react-component-groups/dist/dynamic/WarningModal';
+import useChrome from '@redhat-cloud-services/frontend-components/useChrome';
 import FormButtons from '../shared/FormButtons';
+import pathnames from '../../Utilities/pathnames';
 
 const NotificationsTemplate = ({ schema, formFields }) => {
-  const { handleSubmit } = useFormApi();
+  const formApi = useFormApi();
+  const [isVisibleDialog, setVisibleDialog] = useState(false);
+
+  const { chromeHistory } = useChrome();
+  const [triggerExit, setTriggerExit] = useState({
+    confirmed: false,
+    path: '',
+  });
+
+  const handleGoToIntendedPage = useCallback(
+    (location) => chromeHistory.push(location),
+    [chromeHistory]
+  );
+
+  useEffect(() => {
+    const navigationAllowed =
+      !formApi.getState().dirty || triggerExit.confirmed;
+    if (navigationAllowed) {
+      handleGoToIntendedPage(triggerExit.path);
+    }
+    const unblock = chromeHistory.block(({ location }) => {
+      if (
+        !location.pathname?.includes(pathnames.notifications.link) &&
+        formApi.getState().dirty
+      ) {
+        setVisibleDialog(true);
+      }
+      setTriggerExit((obj) => ({ ...obj, path: location.pathname }));
+      return navigationAllowed;
+    });
+
+    return () => {
+      unblock();
+    };
+  }, [
+    handleGoToIntendedPage,
+    chromeHistory,
+    triggerExit.confirmed,
+    triggerExit.path,
+  ]);
 
   return (
-    <form onSubmit={handleSubmit} style={{ display: 'contents' }}>
-      {schema.title}
-      {formFields}
-      <FormButtons />
-    </form>
+    <>
+      <WarningModal
+        isOpen={isVisibleDialog}
+        title="Unsaved changes"
+        onConfirm={() => {
+          setTriggerExit((obj) => ({
+            ...obj,
+            confirmed: true,
+          }));
+          setVisibleDialog(false);
+        }}
+        onClose={() => setVisibleDialog(false)}
+      >
+        Your page contains unsaved changes. Do you want to leave?
+      </WarningModal>
+
+      <form onSubmit={formApi.handleSubmit} style={{ display: 'contents' }}>
+        {schema.title}
+        {formFields}
+        <FormButtons />
+      </form>
+    </>
   );
 };
 
